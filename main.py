4cc59e60f02108fdf5a8708ed6ca2323252a3f6a
studentnes89 <105539200+studentnes89@users.noreply.github.com>
2022-06-09 12:09:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ df2['price'] = df2['price'].str.replace('$','1')
 
 ##Выбираем регион
 df_new = df2[["region", "name", "city", "price", "cuisine"]]
-##Region = st.selectbox(
+Region = st.selectbox(
         "Region", df_new["region"].value_counts().index
     )
 df_selection = df_new[lambda x: (x["region"] == Region]
