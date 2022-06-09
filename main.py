593d24a92dd967bd5e2050e24bb53621a20cf9d5
studentnes89 <105539200+studentnes89@users.noreply.github.com>
2022-06-09 11:39:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ import matplotlib.pyplot as plt
 df= pd.read_csv("data_one.csv")
 df2=df.dropna()
 df2['price'] = df2['price'].str.replace('$','a')
-df_sort = df2.sort_values('price', ascending = TRUE)
+
 
 ##Выбираем регион
-df_new = df_sort[["region", "name", "city", "price", "cuisine"]]
+df_new = df2[["region", "name", "city", "price", "cuisine"]]
 Region = st.selectbox(
         "Region", df_new["region"].value_counts().index
     )
 df_selection = df_new[lambda x: x["region"] == Region]
-df_selection
+df_selection.sort_values('price', ascending = TRUE)
 
 ##Выбираем кухню
 Cuisine = st.selectbox(
