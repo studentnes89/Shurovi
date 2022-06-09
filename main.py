1325a9ff62278961444889bc34fa773177883d84
studentnes89 <105539200+studentnes89@users.noreply.github.com>
2022-06-09 10:59:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ import matplotlib.pyplot as plt
 
 df= pd.read_csv("data_one.csv")
 
-
+##Выбираем регион
 df_new = df[["region", "name", "city", "price", "cuisine"]]
 Region = st.selectbox(
         "Region", df_new["region"].value_counts().index
     )
 df_selection = df_new[lambda x: x["region"] == Region]
 df_selection
+
+##Выбираем кухню
+Cuisine = st.selectbox(
+        "Cuisine", df_new["cuisine"].value_counts().index
+    )
+df_selection2 = df_new[lambda x: x["cuisine"] == Cuisine]
+df_selection2
