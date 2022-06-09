eb4dbad97617025261362a2099ccd855333fdefb
studentnes89 <105539200+studentnes89@users.noreply.github.com>
2022-06-09 10:44:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import streamlit as st
+  import geopandas as gpd
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+ import seaborn as sns
+ from mpl_toolkits.mplot3d import Axes3D
+import folium
+from streamlit_folium import st_folium
+import json
 
+
+st.header("Michelin restaurants")
+df= pd.read_csv("data_one.csv")
