af52fc8547d82b3b3d3812ed7df28c6d8bfbdb56
studentnes89 <105539200+studentnes89@users.noreply.github.com>
2022-06-09 10:44:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import streamlit as st
-  import geopandas as gpd
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
- import seaborn as sns
- from mpl_toolkits.mplot3d import Axes3D
-import folium
-from streamlit_folium import st_folium
-import json
+
+
+
 
 
 st.header("Michelin restaurants")
