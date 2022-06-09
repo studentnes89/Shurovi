99934837fc8ab2191c81789fe5d8e9fb5e431bdb
studentnes89 <105539200+studentnes89@users.noreply.github.com>
2022-06-09 11:41:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ Region = st.selectbox(
         "Region", df_new["region"].value_counts().index
     )
 df_selection = df_new[lambda x: x["region"] == Region]
-df_selection.sort_values('price', ascending = TRUE)
+df_selection_sort = df_selection.sort_values('price', ascending = TRUE)
+df_selection_sort[0:5]
 
 ##Выбираем кухню
 Cuisine = st.selectbox(
