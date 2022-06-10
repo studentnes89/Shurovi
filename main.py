8b728c5ac9a7e6b13c65c26f31b6864323fd0e24
studentnes89 <105539200+studentnes89@users.noreply.github.com>
2022-06-10 20:41:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -81,19 +81,4 @@ for link in t("img"):
         break
 st.write("https:"+ itog)
 url = "https:"+ itog
-r= requests.get(url)
-out = open("...\img.jpg", 'wb')
-out.write(r.content())
-out.close()
-
-uploaded_file = st.file_uploader("...\img.jpg", type="jpg")
-
-if uploaded_file is not None:
-    # Convert the file to an opencv image.
-    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    opencv_image = cv2.imdecode(file_bytes, 1)
-
-    # Now do something with the image! For example, let's display it:
-    st.image(opencv_image, channels="BGR")
-
-
+st.image(url_pic)
