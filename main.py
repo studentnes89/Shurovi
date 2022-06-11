482a6a793ab9ff49f8dc284c7ed00cb7bd219a53
studentnes89 <105539200+studentnes89@users.noreply.github.com>
2022-06-11 10:59:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ st.image(url)
 ###Карта
 
 
-map = folium.Map(location=[37.296933,-121.9574983], zoom_start = 8, tiles = "Mapbox bright")
+map = folium.Map(location=[37.296933,-121.9574983], zoom_start = 8)
 
 folium.Marker(location=[37.4074687,-122.086669], popup = "Google HQ", icon=folium.Icon(color = 'gray')).add_to(map)
 
